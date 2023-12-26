3cfe4863fea67769d79383633b60bbff9ca09412
duyns1110 <115967239+duyns1110@users.noreply.github.com>
2023-12-26 18:03:58 +0700

    Add files via upload
    sắp xếp command theo arrival time cho sẵn trong file txt
    đẩy vào queue
    chờ lệnh, chờ thời gian bắt đầu
    lấy từ queue và thực hiện lệnh
    chưa kiểm tra lại waiting time và turnaround time

--- fcfs.sh
+++ fcfs.sh
@@ -1,5 +1,4 @@
-path=/home/huy/user/CPU_Scheduling_With_Bash/ready_queue.txt
-# Read file line by line
+path=/home/lenovo/Desktop/CPU_Scheduling_with_bash-master/ready_queue.txt
 
 waiting_time() {
     time_start=$1
@@ -51,20 +50,37 @@ convert_sec_to_real_time()
     echo "$h:$m:$s"
 }
 
-while IFS="," read -r command time_arrival
-    do
-        # Do thời gian để  ở dạng giây tính theo thời gian thực nên cần 1 bước chuyển đổi về giờ
-        echo "Arrival time: $(convert_sec_to_real_time $time_arrival)" 
-        time_start=$(date "+%s")
-        echo "Time start: $(convert_sec_to_real_time $time_start)"
-        echo "Waiting time of "$command": $(waiting_time $time_start $time_arrival)"
-        echo "Start command: $command"
-        # Thực hiện lệnh
-        $command
+sorted_lines=$(sort -t',' -k2 -n $path)
 
-        # Hiển thị ra thời gian kết thúc
+declare -a command_queue
+
+while IFS="," read -r command time_arrival
+do
+    current_time=$(date "+%s")
+
+    new_time_arrival=$(($current_time + $time_arrival))
+    echo "Current Time: $(convert_sec_to_real_time $current_time)" 
+    echo "Thời gian đến: $(convert_sec_to_real_time $new_time_arrival)" 
+
+    command_queue+=("$command,$new_time_arrival")
+
+    if [ -z "$running_command" ]; then
+        running_command="${command_queue[0]}"
+        unset 'command_queue[0]'
+
+        IFS=',' read -r queued_command queued_arrival_time <<< "$running_command"
+        
+        sleep $((queued_arrival_time - current_time))
+
+        echo "Bắt đầu lệnh: $queued_command"
+        $queued_command
+
         time_end=$(date "+%s")
-        echo "Time end: $(convert_sec_to_real_time $time_end)"
-        echo "Turn around time of "$command": $(turnaround_time $time_end $time_arrival)"
+        echo "Thời điểm kết thúc: $(convert_sec_to_real_time $time_end)"
+        echo "Thời gian hoàn thành của $queued_command: $(turnaround_time $time_end $queued_arrival_time)"
+        echo "Thời gian chờ của $queued_command: $(waiting_time $time_end $queued_arrival_time)"
         echo "------------------------------------"
-    done < $path
+
+        unset running_command
+    fi
+done <<< "$sorted_lines"
